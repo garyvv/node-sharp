b1ccb964ba725573385e99ab93bb0c6104e1b083
Garyvv <370977448@qq.com>
2018-06-22 17:20:48 +0800

    [DOC] add doc

--- app/controller/catering/customer.js
+++ app/controller/catering/customer.js
@@ -12,6 +12,40 @@ module.exports = {
 		return Response.output(ctx, data)
 	},
 
+	/**
+     * @api {put} /api/catering/v1/customer/:customerId 编辑用户信息
+	 * @apiVersion 1.0.0
+	 * @apiName 编辑用户信息
+     * @apiGroup customer
+     * @apiPermission user
+	 * 
+	 * @apiDescription API to edit the customer information.
+	 * 
+	 * @apiExample Example usage:
+    	curl --request PUT \
+			--url https://garylv.com/api/catering/v1/customers/1 \
+			--header 'mina-source: catering' \
+			--header 'store-id: 0'
+	 * 
+     * @apiParam {String} [nickName] nickName
+     * @apiParam {String} [gender] gender
+     * @apiParam {String} [avatarUrl] avatarUrl
+     * @apiParam {String} [country] country
+     * @apiParam {String} [province] province
+     * @apiParam {String} [city] city
+     * @apiParam {String} [mobile] mobile
+     * @apiParam {String} [language] language
+	 * 
+	 * @apiSuccess {String} data response data
+	 * 
+     * @apiSuccessExample {json} Visual Preview:
+		{
+			"code": 200,
+			"message": "请求成功",
+			"data": {}
+		}
+	 * @apiSampleRequest https://garylv.com/api/catering/v1/customers/:customerId
+     */
 	edit: async function (ctx) {
 		let updateData = {}
 		if (!!ctx.input.nickName) updateData.nickname = ctx.input.nickName
